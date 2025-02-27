11bcd0f942636943d5687f6f533e8097013f6508
claire-gallagher <clairegallagher@sandiego.edu>
2025-02-27 15:30:13 -0800

    updated the bar graph to now connect to the updated json of true observations

--- INaturalist_Scraper/dates_graph.py
+++ INaturalist_Scraper/dates_graph.py
@@ -1,8 +1,14 @@
 import matplotlib.pyplot as plt
 import json
 from collections import Counter
+import os
+import json
 
-with open("observations.json", "r") as file:
+# Change working directory to the 'Data' folder inside the current directory
+os.chdir(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'Data'))
+
+# Now you can open the file in the 'Data' folder
+with open("true_observations.json", "r") as file:
     data = json.load(file)
 
 years = []
