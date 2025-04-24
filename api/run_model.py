8eda88a3566b6a9ab10b5fd22a390b43c7876890
dillon9999 <dtimmer707@gmail.com>
2025-04-24 15:33:26 -0700

    fix API port

--- api/run_model.py
+++ api/run_model.py
@@ -272,7 +272,7 @@ def predict():
 @app.route('/upload', methods=['POST'])
 def upload():
     data = request.get_json()
-        
+    
     try:
         coords = data['coordinates']
         timestamp = data['timestamp']
@@ -341,5 +341,4 @@ def upload_bulk():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8000))
-    app.run(host='0.0.0.0', port=port, debug=False)
+    app.run()
